721ccc7ba6569473792dbf9a739b0f8e93018da8
Efraim LA <efraimlopezamador123@gmail.com>
2020-06-17 09:38:06 -0500

    first commit

---
+++ src/main/java/io/efraim/HelloResource.java
@@ -0,0 +1,16 @@
+package io.efraim;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/token")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
